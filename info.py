e3a56d95e0420427158c840db9a237d163b614ce
muhjasil <jasiljasil000@gmail.com>
2025-10-04 16:46:06 +0530

    info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split()
+PICS = (environ.get('PICS', 'https://i.ibb.co/VpVkqHDL/x.jpg')).split()
 
 
 # Admins & Users
@@ -194,6 +194,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
